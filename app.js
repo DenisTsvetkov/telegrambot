9f4973fb4a5ccb6d73c55e921f70409ec13d73b4
Denis Tsvetkov <denis.tsvetkov59@gmail.com>
2019-01-20 04:16:22 +0500

    first

---
+++ app.js
@@ -0,0 +1,58 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '571499408:AAEtxaMQ6RqbdpYK80SSa_T0vYNvLASLQ3g';
+
+const bot = new TelegramBot(token, {polling: true});
+
+let users = [];
+
+
+bot.onText(/\/start/, (msg, match) => {
+
+  const chatId = msg.chat.id;
+  
+  users[chatId]= {
+    'uid': msg.from.id,
+    'chatId': msg.chat.id,
+    'ufirstname': msg.chat.first_name,
+    'ulastname': msg.chat.last_name
+  };
+
+  bot.sendMessage(chatId, 'Главное меню', {
+    reply_markup: {
+      keyboard:[
+        [
+          {text: 'Расписание'},
+          {text: 'О клубе'},
+          {text: 'Задать вопрос'},
+          {text: 'Заказать звонок'}
+        ]
+      ],
+      resize_keyboard: true
+    }
+  });
+});
+
+bot.onText(/Расписание/, (msg, match) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'АКТУАЛЬНОЕ РАСПИСАНИЕ');
+});
+
+bot.onText(/О клубе/, (msg, match) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Инфа о клубе');
+});
+
+bot.onText(/Задать вопрос/, (msg, match) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Задать вопрос!');
+});
+
+
+
+// setInterval(function(){
+//   users.forEach(function callback(value, index, users) {
+//     bot.sendMessage(users[index]['chatId'], JSON.stringify(value));
+//   });
+// },5000);
+
